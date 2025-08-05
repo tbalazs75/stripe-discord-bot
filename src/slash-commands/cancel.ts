57c459cf7df391148976610117f9c3a63ff71ef3
tbalazs75 <lepjszintetbalazs@gmail.com>
2025-08-05 18:38:31 +0200

    Update cancel.ts

--- src/slash-commands/cancel.ts
+++ src/slash-commands/cancel.ts
@@ -55,3 +55,54 @@ export const run = async (interaction: ChatInputCommandInteraction) => {
   if (!active) {
     return void interaction.reply({
       ephemeral: true,
+      embeds: errorEmbed(`You don't have an active subscription!`).embeds,
+    });
+  }
+
+  const confirmEmbed = new EmbedBuilder()
+    .setAuthor({ name: `${user.tag} cancellation`, iconURL: user.displayAvatarURL() })
+    .setDescription(`Are you sure you want to cancel your subscription?`)
+    .setColor(process.env.EMBED_COLOR);
+
+  const randomId = Math.floor(Math.random() * 900) + 100;
+  const customId = `cancel-confirm-${user.id}-${randomId}`;
+
+  const components = [
+    new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setCustomId(customId)
+        .setLabel("Confirm")
+        .setStyle(ButtonStyle.Danger)
+    ),
+  ];
+
+  await interaction.reply({ ephemeral: true, embeds: [confirmEmbed], components });
+
+  const channel = interaction.channel;
+  if (!channel || !channel.isTextBased()) return;
+
+  const collector = channel.createMessageComponentCollector({
+    filter: (i) => i.customId === customId,
+    time: 1000 * 60 * 5,
+  });
+
+  collector.on("collect", async (_i) => {
+    const i = _i as ButtonInteraction;
+
+    if (!i.isButton()) return;
+    if (i.customId !== customId) return;
+
+    await cancelSubscription(active.id);
+
+    const embed = new EmbedBuilder()
+      .setAuthor({ name: `${user.tag} cancellation`, iconURL: user.displayAvatarURL() })
+      .setDescription(`We're sorry to see you go! Your subscription has been cancelled.`)
+      .setColor(process.env.EMBED_COLOR);
+
+    await i.reply({ ephemeral: true, embeds: [embed], components: [] });
+  });
+
+  collector.on("end", async () => {
+    await interaction.editReply({ embeds: [confirmEmbed], components: [] });
+  });
+};
