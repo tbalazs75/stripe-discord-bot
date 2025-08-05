ec777c78e88d35b941a10e0b6d623a8e0477da60
tbalazs75 <lepjszintetbalazs@gmail.com>
2025-08-05 17:55:19 +0200

    Update ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,13 +1,10 @@
 import { MessageCommandRunFunction } from "../handlers/commands";
-import { isTextBasedChannel } from "../utils/discord"; // csak ha van ilyen függvényed
 import { TextChannel } from "discord.js";
 
-export const commands = [
-    'ping'
-];
+export const commands = ['ping'];
 
 export const run: MessageCommandRunFunction = async (message) => {
-    if (message.channel && message.channel.isTextBased && message.channel.isTextBased()) {
+    if (message.channel && message.channel.isTextBased) {
         message.channel.send(`🏓 Pong! My latency is currently \`${message.client.ws.ping}ms\`.`);
     }
 }
