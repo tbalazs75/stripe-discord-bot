ebfaf0aecfa5ca33228e4eafd95db4b1ce22deca
tbalazs75 <lepjszintetbalazs@gmail.com>
2025-08-05 18:36:30 +0200

    Update admin-access.ts

--- src/slash-commands/admin-access.ts
+++ src/slash-commands/admin-access.ts
@@ -1,78 +1,78 @@
-import { SlashCommandRunFunction } from "../handlers/commands";
-import fetch from 'node-fetch';
+import { ChatInputCommandInteraction, PermissionsBitField } from "discord.js";
 import { errorEmbed, successEmbed } from "../util";
-import { DiscordCustomer, Postgres } from "../database";
-import { ApplicationCommandOptionType, CommandInteractionOptionResolver, GuildMember, PermissionsBitField } from "discord.js";
-import { findActiveSubscriptions, findSubscriptionsFromCustomerId, getCustomerPayments, getLifetimePaymentDate, resolveCustomerIdFromEmail } from "../integrations/stripe";
+import { Postgres, DiscordCustomer } from "../database";
 
 export const commands = [
-    {
-        name: "admin-access",
-        description: "Give admin access to a user",
+  {
+    name: "admin-access",
+    description: "Give admin access to a user",
+    options: [
+      {
+        name: "enable",
+        description: "Enable access for the user",
+        type: 1, // Subcommand
         options: [
-            {
-                name: "enable",
-                description: "Enable access for the user",
-                type: ApplicationCommandOptionType.Subcommand,
-                options: [
-                    {
-                        name: "user",
-                        description: "The user you want to give access to",
-                        type: ApplicationCommandOptionType.User,
-                        required: true
-                    }
-                ]
-            },
-            {
-                name: "disable",
-                description: "Disable access for the user",
-                type: ApplicationCommandOptionType.Subcommand,
-                options: [
-                    {
-                        name: "user",
-                        description: "The user you want to remove access from",
-                        type: ApplicationCommandOptionType.User,
-                        required: true
-                    }
-                ]
-            }
+          {
+            name: "user",
+            description: "The user you want to give access to",
+            type: 6, // User
+            required: true
+          }
         ]
-    }
+      },
+      {
+        name: "disable",
+        description: "Disable access for the user",
+        type: 1,
+        options: [
+          {
+            name: "user",
+            description: "The user you want to remove access from",
+            type: 6,
+            required: true
+          }
+        ]
+      }
+    ]
+  }
 ];
 
-export const run: SlashCommandRunFunction = async (interaction) => {
+export const run = async (interaction: ChatInputCommandInteraction) => {
+  await interaction.deferReply();
 
-    await interaction.deferReply();
+  if (!interaction.memberPermissions?.has(PermissionsBitField.Flags.Administrator)) {
+    return void interaction.followUp(
+      errorEmbed("This command needs privileged access and can only be used by administrators.")
+    );
+  }
 
-    if (!interaction.memberPermissions?.has(PermissionsBitField.Flags.Administrator)) {
-        return void interaction.followUp(errorEmbed("This command needs privileged access and can only be used by administrators."));
-    }
+  const subCommand = interaction.options.getSubcommand();
+  const user = interaction.options.getUser("user", true);
 
-    const subCommand = (interaction.options as CommandInteractionOptionResolver).getSubcommand()!;
-    const user = (interaction.options as CommandInteractionOptionResolver).getUser("user")!;
+  const userCustomer = await Postgres.getRepository(DiscordCustomer).findOne({
+    where: { discordUserId: interaction.user.id }
+  });
 
-    const userCustomer = await Postgres.getRepository(DiscordCustomer).findOne({
-        where: {
-            discordUserId: interaction.user.id
-        }
+  if (userCustomer) {
+    await Postgres.getRepository(DiscordCustomer).update(userCustomer.id, {
+      adminAccessEnabled: subCommand === "enable"
     });
-
-    if (userCustomer) await Postgres.getRepository(DiscordCustomer).update(userCustomer.id, {
-        adminAccessEnabled: subCommand === "enable"
+  } else {
+    await Postgres.getRepository(DiscordCustomer).insert({
+      discordUserId: user.id,
+      adminAccessEnabled: subCommand === "enable"
     });
-    else await Postgres.getRepository(DiscordCustomer).insert({
-        discordUserId: user.id,
-        adminAccessEnabled: subCommand === "enable"
-    });
+  }
 
-    const member = interaction.guild?.members.cache.get(user.id);
+  const member = interaction.guild?.members.cache.get(user.id);
 
-    if (subCommand === "enable") {
-        if (member) await member.roles.add(process.env.ADMIN_ROLE_ID!);
-    } else {
-        if (member) await member.roles.remove(process.env.ADMIN_ROLE_ID!);
-    }
+  if (subCommand === "enable") {
+    if (member) await member.roles.add(process.env.ADMIN_ROLE_ID!);
+  } else {
+    if (member) await member.roles.remove(process.env.ADMIN_ROLE_ID!);
+  }
 
-    return void interaction.followUp(successEmbed(`Successfully ${subCommand === "enable" ? "enabled" : "disabled"} access for ${user.tag}.`));
-    
-}
+  return void interaction.followUp(
+    successEmbed(`Successfully ${subCommand === "enable" ? "enabled" : "disabled"} access for ${user.tag}.`)
+  );
+};
