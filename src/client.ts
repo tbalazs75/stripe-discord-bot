835f76983c00d2ec8c43e5cfdf88f5ea34acb9ef
tbalazs75 <lepjszintetbalazs@gmail.com>
2025-08-10 19:59:26 +0200

    Create client.ts

---
+++ src/client.ts
@@ -0,0 +1,9 @@
+import { Client, IntentsBitField } from "discord.js";
+
+export const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMembers,
+  ],
+});
